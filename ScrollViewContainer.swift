1358092791005b9e231466aedc6829d9221a627a
Kan Chen <kan@drchrono.com>
2016-01-04 16:59:00 -0800

    FinishedScrollView

--- ScrollViewContainer.swift
+++ ScrollViewContainer.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ScrollViewContainer: UIView {
 
+    @IBOutlet var scrollView: UIScrollView!
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
